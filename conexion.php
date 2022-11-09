19986aff6527eb084d1b3a82c7705513a51920e9
samuel <87429427+samjes10@users.noreply.github.com>
2022-11-09 18:43:25 -0400

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,9 +1,9 @@
 <?php 
 #-*CONEXION PARA LA BASE DE DATOS XAMPP*-
-$servername = "localhost";
-$database = "maxibasquet";
-$username = "root";
-$password = "root123";
+$servername = "ec2-44-207-253-50.compute-1.amazonaws.com";
+$database = "d77ac7v76aafrj";
+$username = "wlegllgkonwreq";
+$password = "9aa04b445e9f2fc1ef632ac242cad2683c45b439e936914e4edc954d5b43787e";
 // Create connection
 $link = new mysqli($servername,$username,$password,$database) or die("Error ".mysql_error($link));
 ?>

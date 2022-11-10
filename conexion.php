a97af3204a13549db98a1a832e95087df4b83e87
samuel <87429427+samjes10@users.noreply.github.com>
2022-11-10 01:07:05 -0400

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,9 +1,10 @@
 <?php 
 #-*CONEXION PARA LA BASE DE DATOS XAMPP*-
-$servername = "ec2-44-207-253-50.compute-1.amazonaws.com";
-$database = "d77ac7v76aafrj";
-$username = "wlegllgkonwreq";
-$password = "9aa04b445e9f2fc1ef632ac242cad2683c45b439e936914e4edc954d5b43787e";
+$Host = "ec2-44-207-253-50.compute-1.amazonaws.com";
+$Database = "d77ac7v76aafrj";
+$User = "wlegllgkonwreq";
+$Port = "5432"
+$Password = "9aa04b445e9f2fc1ef632ac242cad2683c45b439e936914e4edc954d5b43787e";
 // Create connection
 $link = new mysqli($servername,$username,$password,$database) or die("Error ".mysql_error($link));
 ?>
